f9ab428351cc04f1b05c1347a88975b7463817f3
AliakseiShylovich <131357751+AliakseiShylovich@users.noreply.github.com>
2023-05-21 14:40:25 +0300

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,11 +1,35 @@
+import java.math.BigInteger;
+
 public class Main {
     public static void main(String[] args) {
-        int[] array = {0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144,};
-        int sum = 0;
-        int indexEnd = 3;
-        for (int index = 3; index <= indexEnd; index += 3) {
-            sum = sum + array[index];
-        }
-        System.out.println(sum);
+        int index = -112;     // создаем переменную хранящую индекс числа в последовательности Фибоначчи
+        int evennumber = 3;   // создаем переменную хранящую индекс четного числа
+        BigInteger first = new BigInteger("0");     // первое число в последовательности
+        BigInteger second = new BigInteger("1");    // второе число в последовательности
+        BigInteger third;                               // хранит значение суммы двух предыдущих чисел
+        BigInteger sumFib = new BigInteger("0");    // хранит значение суммы четных чисел до [index]
+            if (index >= 0) {
+                for (int i = 2; i <= index; i++) {
+                    third = first.add(second);
+                    first = second;
+                    second = third;
+                    if (i == evennumber) {
+                        evennumber = evennumber + 3;
+                        sumFib = third.add(sumFib);
+                }
+            }
+                System.out.println(sumFib);
+            } else {
+                for (int i = 2; i <= Math.abs(index); i++) {
+                    third = first.subtract(second);
+                    first = second;
+                    second = third;
+                    if (i == evennumber) {
+                        evennumber = evennumber + 3;
+                        sumFib = third.add(sumFib);
+                    }
+                }
+                System.out.println(sumFib);
+            }
     }
 }
